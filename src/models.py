c369977582cd5cf1546f6a5216f0e1a998657d99
httpscammy <109007099+httpscammy@users.noreply.github.com>
2023-12-11 23:18:36 +0000

    Final commit

--- src/models.py
+++ src/models.py
@@ -1,62 +1,53 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
+from datetime import datetime
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'User'
-    id = Column(Integer, primary_key=True)
-    user = Column (String(25), nullable = False)
-    email = Column(String(250), nullable=False)
-    password = Column(String(30), nullable=False)
+  __tablename__ = 'user'
+  id = Column(Integer, primary_key=True)
+  name = Column(String(25), nullable=False)
+  last_name = Column(String(25), nullable=False)
+  email = Column(String(250), nullable=False)
+  password = Column(String(25), nullable=False)
+ 
+class Planets(Base):
+  __tablename__ = 'planets'
+  id = Column(Integer, primary_key=True)
+  name = Column(String(250), nullable=False)
+  diameter = Column(Integer, nullable=False)
+  climate = Column(String(250), nullable=True)
+  population = Column(Integer, nullable=True)
 
-class Character(Base):
-    __tablename__='Character'
-    id = Column(Integer, primary_key = True)
-    name = Column(String(250), nullable=False)
-    eye_color = Column(String(250))
-    height = Column(String(250))
-    birth_year = Column(String(250))
-    gender = Column(String(250))
+class Characters(Base):
+  __tablename__= 'characters'
+  id = Column(Integer, primary_key=True)
+  name = Column(String(250), nullable=True)
+  eye_color = Column(String(250), nullable=True)
+  birth_year= Column(Integer, nullable=True)
+  gender = Column(String(250), nullable=True)
 
+class Vehicles(Base):
+  __tablename__ = 'vehicles'
+  id = Column(Integer, primary_key=True)
+  model = Column(String(250), nullable=True)
+  length = Column(Integer, nullable=True)
+  crew = Column(Integer, nullable=True)
+  manufactor = Column(String(250), nullable=True)
 
-class Planet(Base):
-    __tablename__= 'Planet'
-    id = Column(Integer, primary_key = True)
-    name = Column(String(250), nullable=False)
-    climate = Column(String(250))
-    terrain = Column(String(250))
-    population = Column(Integer)
-    diameter = Column(Integer)
-
-class Vehicle(Base):
-    __tablename__ = "Vehicle"
-    id = Column(Integer, primary_key = True)
-    model = Column(String(250), nullable=False)
-    manufacturer = Column(String(250))
-    cost_in_credits = Column(Integer)
-    Crew = Column(Integer)
 
 class Favorites(Base):
-    __tablename__ = 'Favorites'
-    id = Column(Integer, primary_key = True)
-    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
-    character_id = Column(Integer, ForeignKey('character.id'), nullable=False, primary_key=True)
-    planet_id = Column(Integer, ForeignKey('planet.id'), nullable=False, primary_key=True)
-    vehicle_id = Column(Integer, ForeignKey('vehicle.id'), nullable=False, primary_key=True)
+  __tablename__= 'favorites'
+  id = Column(Integer, primary_key=True)
+  user_id = Column(Integer, ForeignKey('user.id'))
+  planet_id = Column(Integer, ForeignKey('planets.id'))
+  character_id = Column(Integer, ForeignKey('characters.id'))
+  vehicles_id = Column(Integer, ForeignKey('vehicles.id'))
 
-    # Relationships
-    User.Favorites = relationship("Favorite", back_populates="user")
-    Planet.Favorites = relationship("Favorite", back_populates="planet")
-    Character.Favorites = relationship("Favorite", back_populates="character")
-    Vehicle.Favorites = relationship("Favorite", back_populates="vehicle")
-
-    def to_dict(self):
-        return {}
-
-## Draw from SQLAlchemy base
+# Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
