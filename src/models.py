f155bf4eeaa25857283ce554fecf76176bb36ac1
httpscammy <109007099+httpscammy@users.noreply.github.com>
2023-12-11 04:38:41 +0000

    First commit

--- src/models.py
+++ src/models.py
@@ -7,23 +7,50 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'User'
+    user = Column (String(250), nullable = False)
+    email = Column(Integer, primary_key=True)
+    password = Column(String(250), nullable=False)
+    favorite_planets = relationship('Planets')
+    favorite_characters = relationship('Character')
+    favorite_vehicles = relationship('Vehicles')
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Favorites(Base):
+    __tablename__ = 'Favorites'
+    id = Column(Integer, primary_key = True)
+    favorite_id = Column(Integer, )
+    name = Column(String(250))
+
+class Characters(Base):
+    __tablename__='Characters'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250))
+    eye_color = Column(String(250))
+    height = Column(String(250))
+    birth_year = Column(String(250))
+    gender = Column(String(250))
+
+
+class Planets(Base):
+    __tablename__= 'Planets'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250))
+    climate = Column(String(250))
+    terrain = Column(String(250))
+    population = Column(Integer)
+    diameter = Column(Integer)
+
+
+
+class Vehicles(Base):
+    __tablename__ = "Vehicles"
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250))
+    model = Column(String(250))
+    manufacturer = Column(String(250))
+    cost_in_credits = Column(Integer)
+    Crew = Column(Integer)
 
     def to_dict(self):
         return {}
